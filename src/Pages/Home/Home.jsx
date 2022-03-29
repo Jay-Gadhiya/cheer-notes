9bdbf032a214bee63c831325287959abe1c4abf3
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-03-29 12:08:03 +0530

    refactor : main page css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -37,6 +37,7 @@ const HomePage = () => {
                     <div className="notes-cards-container">
                         <NotesCard />
                         <NotesCard />
+                        <NotesCard />
                     </div>
                 </main>
             </div>
