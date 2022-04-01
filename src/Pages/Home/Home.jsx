22112e38119bb344a5b455b4abdf6e3d136bd995
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-04-01 13:49:29 +0530

    refactored code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -121,5 +121,3 @@ export const HomePage = () => {
         </>
     )
 }
-
-// export { HomePage };
--- src/Utilities-Functions/editNote.jsx
+++ src/Utilities-Functions/editNote.jsx
@@ -4,7 +4,6 @@ const editNote = async (note, authState, noteDispatch, setUserNote) => {
     
     try {
         const res = await axios.post(`/api/notes/${note._id}`, { note }, { headers : { authorization: authState.token } });
-        console.log("edited",res);
         if(res.status === 201) {
             noteDispatch({type : "EDIT_NOTE", payload : res.data.notes });
             setUserNote(pre => ({ ...pre, title : "", content : "", color : "",flag : false }));
