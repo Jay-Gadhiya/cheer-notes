067d28dacbf2580317e79a32c3c11f0302b23a8c
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-04-01 13:39:34 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
 import { LandingPage } from "./Pages/Landing-page/LandingPage";
 import { HomePage } from "./Pages/Home/Home";
 import { Login } from "./Pages/Authentication/Login";
